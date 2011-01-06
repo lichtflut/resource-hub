e45ac25df768aca9599d1999b2b697dd6113a75f
otigges <otigges@lichtflut.de>
2011-01-06 21:19:15 +0100

    added javacc/jjtree for rb template declarations

---
+++ core/src/main/java/de/lichtflut/rb/core/rbdecl/RbdeclNode.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2011 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
+ */
+package de.lichtflut.rb.core.rbdecl;
+
+/**
+ * <p>
+ *  [DESCRIPTION]
+ * </p>
+ *
+ * <p>
+ * 	Created Jan 6, 2011
+ * </p>
+ *
+ * @author Oliver Tigges
+ */
+public class RbdeclNode {
+
+	public String name;
+	
+}
