e026f82147c0439e036961ef974627084fea172a
Nils Bleisch <Nils.Bleisch@innoq.com>
2011-02-21 15:34:52 +0100

    Removed ImplBase from ConstaintFactory

--- core/src/main/java/de/lichtflut/rb/core/schema/impl/ConstraintFactory.java
+++ core/src/main/java/de/lichtflut/rb/core/schema/impl/ConstraintFactory.java
@@ -4,30 +4,6 @@ import org.arastreju.sge.model.ResourceID;
 
 import de.lichtflut.rb.core.schema.Constraint;
 
-public final class ConstraintFactory implements Constraint {
-
-	
-	
-	
-	
-	public String getLiteralConstraint() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public ResourceID getResourceTypeConstraint() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean isLiteralConstraint() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean isResourceTypeConstraint() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+public final class ConstraintFactory{
 
 }
