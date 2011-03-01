7e9e77b59a28f49d7482f62d770f41e4d00ad133
Nils Bleisch <Nils.Bleisch@innoq.com>
2011-03-01 20:29:30 +0100

    Added at least one valid constructor to ResourceSchemaImpl

--- core/src/main/java/de/lichtflut/rb/core/schema/model/impl/ResourceSchemaImpl.java
+++ core/src/main/java/de/lichtflut/rb/core/schema/model/impl/ResourceSchemaImpl.java
@@ -6,8 +6,9 @@ package de.lichtflut.rb.core.schema.model.impl;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+
 import org.arastreju.sge.model.ResourceID;
-import org.arastreju.sge.model.nodes.SNResource;
+import org.arastreju.sge.model.SimpleResourceID;
 
 import de.lichtflut.rb.core.schema.model.PropertyDeclaration;
 import de.lichtflut.rb.core.schema.model.ResourceSchema;
@@ -26,9 +27,9 @@ import de.lichtflut.rb.core.schema.model.ResourceSchema;
 public class ResourceSchemaImpl implements ResourceSchema{
 
 	//Instance members
-	
 	private ResourceID resource;
-	private List<PropertyDeclaration> list = new LinkedList<PropertyDeclaration>();
+	//LinkedList is chosen because it's more flexible compared to an index-driven list
+	private final List<PropertyDeclaration> propertyList = new LinkedList<PropertyDeclaration>();
 	
 	// -----------------------------------------------------
 	
@@ -36,40 +37,48 @@ public class ResourceSchemaImpl implements ResourceSchema{
 	
 	//Default constructor
 	public ResourceSchemaImpl(){
-		//TODO Generate resource with UUID
-		//resource = new SNResource(UUID.randomUUID().toString() ,UUID.randomUUID().toString() );
+		//Generates a SimpleResourceID instance with an random UUID for namespace and identifier each
+		this.resource = new SimpleResourceID(
+				UUID.randomUUID().toString(),
+				UUID.randomUUID().toString());
 	}
 	
 	// -----------------------------------------------------
 	
-	//constructor takes as argument an identifier which will help to define the Resource Identifier
+	//Constructor takes as argument an identifier which will help to define the Resource Identifier
 	public ResourceSchemaImpl(final String identifier) throws IllegalArgumentException{
-		//TODO check if identifier is an valid uri, if not generate a valid one, if this is not possible, throw an exception
-		
+		/*TODO check if identifier is an valid uri, if not generate a valid one, if this is not possible, throw an exception???
+		 * Or generate it anyway?
+		 */
 	}
 	
 	public List<PropertyDeclaration> getPropertyDeclarations() {
-		return list;
+		return this.propertyList;
 	}
 
 	// -----------------------------------------------------
-	
+	/* (non-Javadoc)
+	 * @see de.lichtflut.rb.core.schema.model.Constraint#isLiteralConstraint()
+	 */
 	public ResourceID getResourceID() {
 		return this.resource;
 	}
 
 	// -----------------------------------------------------
 	
+	/**
+	 * @return Returns the toString()-representation of ResourceID followed by a list of toString()-PropertyDeclaration separated in '\n'
+	 */
 	@Override
 	public String toString(){
-		StringBuffer s_buffer = new StringBuffer();
-		s_buffer.append(getResourceID().toString() + "\n");
+		StringBuffer sBuffer = new StringBuffer();
+		sBuffer.append(getResourceID().toString() + "\n");
 		for (PropertyDeclaration property : getPropertyDeclarations()) {
-			s_buffer.append(property.toString() + "\n");
+			sBuffer.append(property.toString() + "\n");
 		}
-		return s_buffer.toString();
+		return sBuffer.toString();
 	}
 	
 	// -----------------------------------------------------
 	
-}
+}//End of class ResourceSchemaImpl
