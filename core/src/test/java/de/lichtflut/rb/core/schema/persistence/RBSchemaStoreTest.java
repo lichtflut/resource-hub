d662bf14c72cd1df9e3b52212c32a22eb8616349
otigges <otigges@lichtflut.de>
2011-04-21 11:54:25 +0200

    changed module structure

---
+++ core/src/test/java/de/lichtflut/rb/core/schema/persistence/RBSchemaStoreTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2011 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
+ */
+package de.lichtflut.rb.core.schema.persistence;
+
+import org.arastreju.sge.Arastreju;
+
+/**
+ * <p>
+ *  [DESCRIPTION]
+ * </p>
+ *
+ * <p>
+ * 	Created Apr 21, 2011
+ * </p>
+ *
+ * @author Oliver Tigges
+ */
+public class RBSchemaStoreTest {
+
+	public void testStore() {
+		 final RBSchemaStore store = new RBSchemaStore(Arastreju.getInstance().rootContext());
+		 
+	}
+	
+}
