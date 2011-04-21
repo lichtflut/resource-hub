e9cfbfe198d787a769a5a0ec3a286dc62bc8c7e3
otigges <otigges@lichtflut.de>
2011-04-21 12:00:09 +0200

    modules changes

---
+++ webck/src/main/java/de/lichtflut/rb/web/components/PlaceholderForGit.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2011 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
+ */
+package de.lichtflut.rb.web.components;
+
+/**
+ * <p>
+ *  [DESCRIPTION]
+ * </p>
+ *
+ * <p>
+ * 	Created Apr 21, 2011
+ * </p>
+ *
+ * @author Oliver Tigges
+ */
+public class PlaceholderForGit {
+
+}
