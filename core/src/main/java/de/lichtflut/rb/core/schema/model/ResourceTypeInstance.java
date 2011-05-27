efc74c9c4db4acc90ac5a046b2e27a6cc1c4f655
Nils Bleisch <Nils.Bleisch@innoq.com>
2011-05-27 16:48:55 +0200

    Added some documentation

--- core/src/main/java/de/lichtflut/rb/core/schema/model/ResourceTypeInstance.java
+++ core/src/main/java/de/lichtflut/rb/core/schema/model/ResourceTypeInstance.java
@@ -20,6 +20,7 @@ import org.arastreju.sge.model.nodes.views.ResourceView;
 /**
  * <p>
  * 	Represents an instance of a well defined ResourceType (RT)
+ *  This type is either represented as key_value store and as graph-based ResourceView, including associations and so on.
  * </p>
  * 
  * Created: May 17, 2011
@@ -28,28 +29,30 @@ import org.arastreju.sge.model.nodes.views.ResourceView;
  */
 public abstract class ResourceTypeInstance<T> extends ResourceView implements Serializable{
 
+	/**
+	 * Keys which help to get some meta data for a given attribute name during runtime
+	 */
 	public enum MetaDataKeys{
 		MAX,
 		MIN,
 		CURRENT,
 		TYPE
 	}
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 535252848460633518L;
 
-	
-	
 	//-- CONSTRUCTOR --------------------------------------
 	
-	
+	/**
+	 * Default constructor
+	 */
 	public ResourceTypeInstance(){
 		//do nothing
 	}
-		
 	
+	/**
+	 * Takes the node of this type as argument
+	 * @param id the {@link ResourceNode} id
+	 */
 	public ResourceTypeInstance(ResourceNode id){
 		super(id);
 	}
@@ -66,14 +69,24 @@ public abstract class ResourceTypeInstance<T> extends ResourceView implements Se
 	
 	// -----------------------------------------------------
 	
-	
+	/**
+	 * Get a simple attribute-name representation of the origin ones. This is realized
+	 * by removing the prefix-namespaces. Please note, that this is just designed for display purposes.
+	 * All other methods still require the full qualified and unique attribute name
+	 */
 	public abstract String getSimpleAttributeName(String attribute);
 	
 	// -----------------------------------------------------
 		
 	
 	/**
-	 * 
+	 * @param attribute
+	 * @param value
+	 * @param ticket the reference to an existing value assignment 
+	 * @throws <p>InvalidValueException when the value does not match the required datatype or constraints
+	 * or if the maximum count of possible values is reached (depends on the cardinality).
+	 * of if the ticket does not exists.
+	 * InvalidAttributeException if this attribute does not exists.</p>
 	 */
 	public abstract void addValueFor(String attribute, String value, int ticket) throws RBInvalidValueException, RBInvalidAttributeException;
 	
@@ -86,7 +99,7 @@ public abstract class ResourceTypeInstance<T> extends ResourceView implements Se
 	// -----------------------------------------------------
 	
 	/**
-	 * 
+	 * returns all the attribute-names of this instance
 	 */
 	public abstract Collection<String> getAttributeNames();
 	
@@ -95,7 +108,7 @@ public abstract class ResourceTypeInstance<T> extends ResourceView implements Se
 	
 	
 	/**
-	 * 
+	 * return all the values for a given attribute
 	 */
 	public abstract Collection<T> getValuesFor(String attribute);
 	
@@ -103,7 +116,7 @@ public abstract class ResourceTypeInstance<T> extends ResourceView implements Se
 	
 	
 	/**
-	 * 
+	 *  return all the existing tickets for a given attribute
 	 */
 	public abstract Collection<Integer> getTicketsFor(String attribute);
 	
@@ -111,21 +124,26 @@ public abstract class ResourceTypeInstance<T> extends ResourceView implements Se
 	
 	
 	/**
-	 * 
+	 * returns a {@RBValidator} for a given attribute
 	 */
 	public abstract RBValidator getValidatorFor(String attribute);
 	
 	// -----------------------------------------------------
 	
 	/**
-	 * 
+	 * tries to generate and return a ticket for given attribute.
+	 * @throws <p>InvalidValueException if the maximum count of possible values (tickets) is reached (depends on the cardinality).
+	 * or if the ticket does not exists. InvalidAttributeException if this attribute does not exists.</p>
+	 * @return TODO: reasearch, make a concept!?!?
 	 */
 	public abstract Integer generateTicketFor(String attribute) throws RBInvalidValueException, RBInvalidAttributeException;
 	
 	// -----------------------------------------------------
 	
 	/**
-	 * 
+	 * tries to release and remove an exisiting ticket
+	 * @throws <p>InvalidValueException if the minimum count of possible values (tickets) is reached (depends on the cardinality).
+	 * or if the ticket does not exists. InvalidAttributeException if this attribute does not exists.</p>
 	 */
 	public abstract void releaseTicketFor(String attribute, int ticket ) throws RBInvalidValueException,RBInvalidAttributeException;
 		
@@ -154,7 +172,8 @@ public abstract class ResourceTypeInstance<T> extends ResourceView implements Se
 	// -----------------------------------------------------
 	
 	/**
-	 * 
+	 * Creates associations based on the internal key-value map.
+	 * The new association might replace an existing older one
 	 */
 	public void createAssociations(Context ctx){
 		Association.create(this,RDF.TYPE, getResourceTypeID(),ctx);
