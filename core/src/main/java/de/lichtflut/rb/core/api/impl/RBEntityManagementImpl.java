c2de75315de5c811221a37bb6d161f26febc1569
Nils Bleisch <Nils.Bleisch@innoq.com>
2011-06-16 14:40:50 +0200

    M0re refactorings... :(

--- core/src/main/java/de/lichtflut/rb/core/api/impl/RBEntityManagementImpl.java
+++ core/src/main/java/de/lichtflut/rb/core/api/impl/RBEntityManagementImpl.java
@@ -25,8 +25,10 @@ import de.lichtflut.rb.core.schema.persistence.RBSchemaStore;
 import de.lichtflut.rb.core.schema.persistence.SNResourceSchema;
 
 /**
- * Reference implementation of {@ResourceTypeManagement} for a lucence based neo4j backend
- * 
+ * <p>
+ * Reference implementation of {@ResourceTypeManagement} for a lucence based neo4j backend.
+ * </p>
+ *
  * Created: May 18, 2011
  *
  * @author Nils Bleisch
@@ -35,20 +37,24 @@ import de.lichtflut.rb.core.schema.persistence.SNResourceSchema;
 public class RBEntityManagementImpl implements RBEntityManagement{
 
 	private ArastrejuGate gate;
-	
+
 	// -- CONSTRUCTOR---------------------------------------
-	
+
 	/**
-	 * This is the standard constructor
+	 * <p>
+	 * This is the standard constructor.
+	 * </p>
 	 * @param gate the ArastrejuGate instance which is necessary to store, update and resolve ResourceTypeInstances
 	*/
-	public RBEntityManagementImpl(ArastrejuGate gate){
+	public RBEntityManagementImpl(final ArastrejuGate gate){
 		this.gate = gate;
 	}
-	
+
 	// -----------------------------------------------------
-	
-	public boolean createOrUpdateRTInstance(RBEntityFactory<Object> instance) {
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean createOrUpdateRTInstance(final RBEntityFactory<Object> instance) {
 		try{
 			ModelingConversation mc = gate.startConversation();
 			instance.createAssociations(null);
@@ -60,15 +66,24 @@ public class RBEntityManagementImpl implements RBEntityManagement{
 	}
 
 	// -----------------------------------------------------
-	
-	@SuppressWarnings("unchecked")
-	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(ResourceSchema schema, String filter, SearchContext ctx) {
-		if(filter==null || filter.equals("")) return loadAllResourceTypeInstancesForSchema(schema);
-		if(ctx==null) ctx = SearchContext.CONJUNCT_MULTIPLE_KEYWORDS;
-		filter = getConvertedFilterBySearchContext(filter, ctx);
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(
+			final ResourceSchema schema, final String filter, final SearchContext ctx) {
+		SearchContext context = ctx;
+		String filterString = filter;
+		if(filter==null || filter.equals("")){
+			return loadAllResourceTypeInstancesForSchema(schema);
+		}
+		if(ctx==null){
+			context = SearchContext.CONJUNCT_MULTIPLE_KEYWORDS;
+		}
+		filterString = getConvertedFilterBySearchContext(filterString, context);
 		Collection<RBEntityFactory> output = new ArrayList<RBEntityFactory>();
 		QueryManager qManager = gate.startConversation().createQueryManager();
-		List<ResourceNode> nodes = qManager.findByTag(filter);
+		List<ResourceNode> nodes = qManager.findByTag(filterString);
 		for (ResourceNode resourceNode : nodes) {
 			//Check if this Instance depends on a ResourceSchema
 			Set<SemanticNode> clients = resourceNode.getAssociationClients(RDF.TYPE);
@@ -86,11 +101,13 @@ public class RBEntityManagementImpl implements RBEntityManagement{
 		}
 		return output;
 	}
-	
+
 	// -----------------------------------------------------
-	
-	@SuppressWarnings("unchecked")
-	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(ResourceSchema schema) {
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(final ResourceSchema schema) {
 	      Collection<RBEntityFactory> output = new ArrayList<RBEntityFactory>();
           ModelingConversation mc = gate.startConversation();
           Set<Statement> statements = mc.createQueryManager().findIncomingStatements(schema.getDescribedResourceID());
@@ -102,43 +119,109 @@ public class RBEntityManagementImpl implements RBEntityManagement{
           }
          return output;
 	}
-	
+
 	// -----------------------------------------------------
-	
-	@SuppressWarnings("unchecked")
-	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(Collection<ResourceSchema> schemas) {
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(final Collection<ResourceSchema> schemas) {
 		Collection<RBEntityFactory> output = new ArrayList<RBEntityFactory>();
 	    for (ResourceSchema resourceSchema : schemas) {
 			output.addAll(loadAllResourceTypeInstancesForSchema(resourceSchema));
 		}
 	    return output;
 	}
-	
-	
-	@SuppressWarnings("unchecked")
-	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(Collection<ResourceSchema> schemas, String filter, SearchContext ctx) {
-		if(filter==null || filter.equals("")) return loadAllResourceTypeInstancesForSchema(schemas);
-		//Check if context is still null
-		if(ctx==null) ctx = SearchContext.CONJUNCT_MULTIPLE_KEYWORDS;
+
+	// -----------------------------------------------------
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Collection<RBEntityFactory> loadAllResourceTypeInstancesForSchema(
+			final Collection<ResourceSchema> schemas, final String filter,  final SearchContext ctx) {
+		SearchContext context = ctx;
+		String filterString = filter;
+		if(filter==null || filter.equals("")){
+			return loadAllResourceTypeInstancesForSchema(schemas);
+		}
+		if(ctx==null){
+			context = SearchContext.CONJUNCT_MULTIPLE_KEYWORDS;
+		}
 		Collection<RBEntityFactory> output = new ArrayList<RBEntityFactory>();
 		for (ResourceSchema schema : schemas) {
-			output.addAll(loadAllResourceTypeInstancesForSchema(schema, filter,ctx));
+			output.addAll(loadAllResourceTypeInstancesForSchema(schema, filterString,context));
 		}
 		return output;
 	}
-	
-	// --PRIVATE MEMBER---------------------------------------	
-	
-	
+
+	// -----------------------------------------------------
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public RBEntityFactory loadRBEntity(final QualifiedName qn) {
+		if(qn==null){ return null;}
+		ModelingConversation mc = this.gate.startConversation();
+		ResourceNode node = mc.findResource(qn);
+		if(node==null){ return null;}
+		//Try to find out the related RT's and Schemas
+		Set<Association> type_assocs = node.getAssociations(RDF.TYPE);
+
+		for (Association association : type_assocs) {
+			if(association.getObject().isResourceNode()){
+				Set<Association> potential_schemas =
+					association.getObject().asResource().getAssociations(RBSchema.DESCRIBED_BY);
+				if(potential_schemas.size()<=0){ return null;}
+				//There might be a resource schema, get 'dem!
+				ResourceNode schemaNode = (ResourceNode) new ArrayList<Association>(potential_schemas).get(0).getObject();
+				ResourceSchema schema =
+					(new RBSchemaStore(this.gate).convertResourceSchema(new SNResourceSchema(schemaNode)));
+				//Build the RBEntity based on node and extracted schema
+				RBEntityFactory entity = new RBEntityImpl(schema, node);
+				return entity;
+			}
+		}
+		return null;
+	}
+
+	// -----------------------------------------------------
+
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public RBEntityFactory loadRBEntity(final String nodeIdentifier) {
+		return loadRBEntity(new QualifiedName(nodeIdentifier));
+	}
+
+	// -----------------------------------------------------
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public RBEntityFactory loadRBEntity(final ResourceNode node) {
+		return loadRBEntity(node.getQualifiedName());
+	}
+
+
+	// --PRIVATE MEMBER---------------------------------------
+
+
 	/**
 	 * Helper method to convert the filter in a way that the underlying lucene engine can work with.
+	 * @param filter - the search keywords
+	 * @param ctx - the {@link SearchContxt} which describes your search mechanics and also might modify the keywords
+	 * @return the based on the searchconxted modified filter-string
 	 */
-	private String getConvertedFilterBySearchContext(String filter,SearchContext ctx) {
+	private String getConvertedFilterBySearchContext(final String filter, final SearchContext ctx) {
+		String filterString = filter;
 		switch(ctx){
-			case CONJUNCT_MULTIPLE_KEYWORDS:{
-				filter = filter.replace("*", " ");
-				filter = filter.replace("?", " ");
-				String[] keywords = filter.split(" ");
+			// -----------------------------------------------------
+			case CONJUNCT_MULTIPLE_KEYWORDS:
+				filterString = filterString.replace("*", " ");
+				filterString = filterString.replace("?", " ");
+				String[] keywords = filterString.split(" ");
 				StringBuilder convertedInput = new StringBuilder();
 				for (int i = 0; i < keywords.length; i++) {
 					if(keywords[i].length()>0){
@@ -147,55 +230,13 @@ public class RBEntityManagementImpl implements RBEntityManagement{
 						convertedInput.append(keywords[i]);
 					}
 				}
-				filter=  convertedInput.toString();	
+				filterString=  convertedInput.toString();
 				break;
-			}
+				// -----------------------------------------------------
+			default: break;
 		}
-		return filter;
+		return filterString;
 	}
 
-	// -----------------------------------------------------
-	
-	@SuppressWarnings("unchecked")
-	public RBEntityFactory loadRBEntity(QualifiedName qn) {
-		if(qn==null) return null;
-		ModelingConversation mc = this.gate.startConversation();
-		ResourceNode node = mc.findResource(qn);
-		if(node==null) return null;
-		//Try to find out the related RT's and Schemas
-		Set<Association> type_assocs = node.getAssociations(RDF.TYPE);
-		
-		for (Association association : type_assocs) {
-			
-			if(association.getObject().isResourceNode()){
-				
-				Set<Association> potential_schemas = association.getObject().asResource().getAssociations(RBSchema.DESCRIBED_BY);
-				if(potential_schemas.size()<=0) return null;
-				
-				//There might be a resource schema, get 'dem!
-				ResourceNode schemaNode = (ResourceNode) new ArrayList<Association>(potential_schemas).get(0).getObject();
-				ResourceSchema schema = (new RBSchemaStore(this.gate).convertResourceSchema(new SNResourceSchema(schemaNode)));
-				//Build the RBEntity based on node and extracted schema
-				RBEntityFactory entity = new RBEntityImpl(schema, node);
-				return entity;
-			}
-		}
-		
-		return null;
-	}
 
-	// -----------------------------------------------------
-	
-	@SuppressWarnings("unchecked")
-	public RBEntityFactory loadRBEntity(String nodeIdentifier) {
-		return loadRBEntity(new QualifiedName(nodeIdentifier));		
-	}
-	
-	// -----------------------------------------------------
-
-	@SuppressWarnings("unchecked")
-	public RBEntityFactory loadRBEntity(ResourceNode node) {
-		return loadRBEntity(node.getQualifiedName());
-	}
-	
 }
