6ce7f4bbb350ca90b6b83d91eccc84e92c7e1f13
Nils Bleisch <Nils.Bleisch@innoq.com>
2011-06-21 12:00:10 +0200

    Dancing with tears in yar eyes.

--- webck/src/main/java/de/lichtflut/rb/web/ck/components/CKComponent.java
+++ webck/src/main/java/de/lichtflut/rb/web/ck/components/CKComponent.java
@@ -3,19 +3,21 @@
  */
 package de.lichtflut.rb.web.ck.components;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
 
 import de.lichtflut.rb.core.spi.RBServiceProvider;
 import de.lichtflut.rb.web.ck.behavior.CKBehavior;
 
 /**
- * [TODO Insert description here.
  * <p>
- *  Basic interface of the generic-resource components-family.
- *  Specifies some methods which have to be implemented
+ *  This is the root of the {@CKComponent}-family.
+ *  This class is abstract and describes how to use or interact with a {@CKComponent},
+ *  how should they look and act like and and what should they do in several defined cases.
  * </p>
  *
  * Created: Jun 20, 2011
@@ -25,6 +27,27 @@ import de.lichtflut.rb.web.ck.behavior.CKBehavior;
 public abstract class CKComponent extends Panel {
 
 	private static final long serialVersionUID = 1L;
+
+	//Behavior-Keys
+	/**
+	 * <p>
+	 * This behavior is called, when the css-style is set during the initialization progress.
+	 * If not set, no special style assertions will be made.
+	 * </p>
+	 * <p>
+	 * The expected return values are a {@link File} or {@link String} referencing the path of the css-file
+	 * or a simple {@link String} which should contain embedded css assertions. If the value does not match the expected types
+	 * or if the value is not a valid existing file e.g., no special style assertion will be made.
+	 * </p>
+	 * <p>
+	 * The following params will be parsed to the behavior in a well-defined order:
+	 * <ol>
+	 * 	<li>{@link Class}, the ck-components-class</li>
+	 * </ol>
+	 * </p>
+	 */
+	public static final String STYLE_BEHAVIOR = "de.lichtflut.web.ck.behavior.style";
+
 	private Map<String, CKBehavior> behaviorRegistry = new HashMap<String, CKBehavior>();
 
 	/**
@@ -47,6 +70,35 @@ public abstract class CKComponent extends Panel {
 
 	// -----------------------------------------------------
 
+	/**
+	 * TODO: This method is not yet ready.
+	 */
+	@SuppressWarnings("unused")
+	private void initStyle(){
+		CKBehavior behavior = getBehavior(STYLE_BEHAVIOR);
+		if(behavior==null){
+			return;
+		}
+		Object css_output = behavior.execute(new Object[]{this.getClass()});
+		if(css_output==null){
+			return;
+		}
+		if(css_output instanceof File){
+			if(!((File) css_output).exists()){
+				return;
+			}
+			ResourceReference css_reference = new CssResourceReference(this.getClass(), css_output.toString());
+		}else if(css_output instanceof String){
+			//Check if the string might be a File
+			if(new File(css_output.toString()).exists()){
+				return;
+			}else{ //The string might be an embedded css-string
+				String embedded_css = css_output.toString();
+			}
+		}
+	}
+
+	// -----------------------------------------------------
 
 	/**
 	 * Returns ServiceProvider.
@@ -55,15 +107,17 @@ public abstract class CKComponent extends Panel {
 	 */
 	public abstract RBServiceProvider getServiceProvider();
 
+	// -----------------------------------------------------
 
 	/**
-	 * Sets the view mode for the component. This method musst be overridden.
+	 * Sets the view mode for the component. This method must be overridden.
 	 * If the ViewMode is not supported, an {@link UnsupportedOperationException} is raised.
 	 * @param mode -
 	 * @return -
 	 */
-
 	public abstract CKComponent setViewMode(final ViewMode mode);
+
+	// -----------------------------------------------------
 
 	/**
 	 * Adds a behavior to a component.
@@ -76,6 +130,8 @@ public abstract class CKComponent extends Panel {
 		return this;
 	}
 
+	// -----------------------------------------------------
+
 	/**
 	 * Returns the behavior for the passed Key.
 	 * @param key - Name of the behavior
@@ -85,6 +141,8 @@ public abstract class CKComponent extends Panel {
 		return behaviorRegistry.get(key);
 	}
 
+	// -----------------------------------------------------
+
 	/**
 	 * Removes a behavior for this component.
 	 * @param key - Name of the behavior which is to be removed
@@ -93,6 +151,8 @@ public abstract class CKComponent extends Panel {
 	public final CKBehavior removeBehavior(final String key){
 		return behaviorRegistry.remove(key);
 	}
+
+	// -----------------------------------------------------
 
 	/**
 	 * Clears all behaviors for this component.
