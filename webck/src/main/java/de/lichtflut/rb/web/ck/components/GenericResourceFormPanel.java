33403ea6da2f71e41e09ae792363a666d9513e58
Ravi Knox <rknox@lichtflut.de>
2011-07-14 18:21:28 +0200

    added Ajax fields for custom key-value pairs

--- webck/src/main/java/de/lichtflut/rb/web/ck/components/GenericResourceFormPanel.java
+++ webck/src/main/java/de/lichtflut/rb/web/ck/components/GenericResourceFormPanel.java
@@ -3,22 +3,32 @@
  */
 package de.lichtflut.rb.web.ck.components;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.RefreshingView;
 import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.markup.repeater.util.ModelIteratorAdapter;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.arastreju.sge.model.ElementaryDataType;
 import org.arastreju.sge.model.ResourceID;
 
@@ -27,6 +37,8 @@ import de.lichtflut.rb.core.schema.model.Constraint;
 import de.lichtflut.rb.core.schema.model.PropertyAssertion;
 import de.lichtflut.rb.core.schema.model.RBEntity;
 import de.lichtflut.rb.core.schema.model.RBEntity.MetaDataKeys;
+import de.lichtflut.rb.core.schema.model.RBInvalidAttributeException;
+import de.lichtflut.rb.core.schema.model.RBInvalidValueException;
 import de.lichtflut.rb.core.schema.model.ResourceSchema;
 import de.lichtflut.rb.web.behaviors.DatePickerBehavior;
 import de.lichtflut.rb.web.components.validators.GenericResourceValidator;
@@ -95,7 +107,7 @@ public abstract class GenericResourceFormPanel extends CKComponent {
 		this.add(new FeedbackPanel("feedback").setEscapeModelStrings(false));
 
 		if (schema != null) {
-			RepeatingView view = new RepeatingView("propertylist");
+			final RepeatingView view = new RepeatingView("propertylist");
 			for (final String attribute : (Collection<String>) instance
 					.getAttributeNames()) {
 				boolean required = true;
@@ -127,9 +139,77 @@ public abstract class GenericResourceFormPanel extends CKComponent {
 				}
 			}
 			form.add(view);
+			final WebMarkupContainer container = new WebMarkupContainer("customValueContainer");
+			container.setOutputMarkupId(true);
+			
+			final RepeatingView customview = new RepeatingView("customValues");
+
+			container.add(customview);
+			AjaxButton button = new AjaxButton("customInput", Model.of("Add Vaules!")) {
+
+				@Override
+				protected void onSubmit(final AjaxRequestTarget target, final Form<?> form) {
+					System.out.println("HALLO +++++++++++++++++++++++++++++++++++++++++++");
+					buildCustomItem(instance, form, target);
+				}
+
+				@Override
+				protected void onError(final AjaxRequestTarget target, final Form<?> form) {
+					System.out.println("ERROR +++++++++++++++++++++++++++++++++++++++++++");
+					// TODO: ADD GenericResourceForm parameter!!!!!!
+					Fragment fragment = new Fragment(customview.newChildId(), "addNonSchemaValues", this.getParent().getParent(), null);
+					fragment.setOutputMarkupId(true);
+					fragment.add(new TextField("key"));
+					fragment.add(new TextField("value"));
+					customview.add(fragment);
+					target.add(container);
+					target.focusComponent(fragment);
+				}
+			};
+			form.add(container);
+			form.add(button);
 		}// End of if(schema==null)
 
 	}// End of Method init
+
+	/**
+	 * Appends GenericResourceForm to add custom fields.
+	 * @param instance -
+	 * @param form -
+	 * @param target -
+	 * @return {@link Component}
+	 */
+	private void buildCustomItem(final RBEntity instance, Form<?> form, AjaxRequestTarget target) {
+//		final RepeatingView view = new RepeatingView("customInput");
+//		final Fragment fragment = new Fragment(view.newChildId(), "customProperties", this);
+//		
+//		Fragment f = new Fragment(view.newChildId(), "addNonSchemaValues", this);
+//		f.add(new TextField("key"));
+//		f.add(new TextField("value"));
+//		view.add(f);
+//		
+//		AjaxButton button = new AjaxButton("addCustomFields") {
+//
+//			@Override
+//			protected void onSubmit(final AjaxRequestTarget target, final Form<?> form) {
+//				Fragment f = new Fragment(view.newChildId(), "addNonSchemaValues", this);
+//				f.add(new TextField("key"));
+//				f.add(new TextField("value"));
+//				view.add(f);
+////				view.modelChanged();
+//			}
+//
+//			@Override
+//			protected void onError(final AjaxRequestTarget target, final Form<?> form) {
+//				// TODO Auto-generated method stub
+//			}
+//		};
+//		// TODO Auto-generated method stub
+//		button.add(new Label("linkLabel", "add additional values"));
+//		fragment.add(button);
+//		view.add(fragment);
+		System.out.println("########################################################");
+	}
 
 	// -----------------------------------------------------
 
