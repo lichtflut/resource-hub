04863c71f534f2d57961fdcd1cd7987acd04f4d3
Raphael Esterle <raphael@ThinkPad.(none)>
2011-12-13 15:59:33 +0100

    added JUnit test for schema2graphBinding

---
+++ core/src/test/java/de/lichtflut/rb/core/schema/persistence/Schema2GraphTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2011 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
+ */
+
+package de.lichtflut.rb.core.schema.persistence;
+
+import junit.framework.Assert;
+
+import org.apache.lucene.util.NumericUtils.IntRangeBuilder;
+import org.arastreju.sge.model.ElementaryDataType;
+import org.arastreju.sge.model.SimpleResourceID;
+import org.arastreju.sge.model.associations.Association;
+import org.junit.Test;
+
+import de.lichtflut.rb.core.RBConfig;
+import de.lichtflut.rb.core.schema.RBSchema;
+import de.lichtflut.rb.core.schema.model.ResourceSchema;
+import de.lichtflut.rb.core.schema.model.impl.CardinalityBuilder;
+import de.lichtflut.rb.core.schema.model.impl.ConstraintBuilder;
+import de.lichtflut.rb.core.schema.model.impl.FieldLabelDefinitionImpl;
+import de.lichtflut.rb.core.schema.model.impl.PropertyDeclarationImpl;
+import de.lichtflut.rb.core.schema.model.impl.ResourceSchemaImpl;
+import de.lichtflut.rb.core.schema.model.impl.TypeDefinitionImpl;
+import de.lichtflut.rb.core.security.impl.MockLoginService;
+import de.lichtflut.rb.core.services.ServiceProvider;
+import de.lichtflut.rb.core.services.impl.DefaultRBServiceProvider;
+
+/**
+ * <p>
+ *  Test case for converting Resource Schemas to Arastreju Graphs.
+ * </p>
+ *
+ * <p>
+ * 	Created Dez 10, 2011
+ * </p>
+ *
+ * @author Raphael Esterle
+ */
+
+public class Schema2GraphTest {
+	
+	@Test
+	public void testTest(){
+		ResourceSchema schema = createSchema();
+		Assert.assertNotNull(schema);
+		
+		Schema2GraphBinding b = new Schema2GraphBinding(null);
+		
+		SNResourceSchema snr = b.toSemanticNode(schema);
+		
+		Assert.assertNotNull(snr);
+		Assert.assertTrue(snr.getDescribedType().equals(schema.getDescribedType()));
+		Assert.assertTrue(snr.getPropertyDeclarations().size()==schema.getPropertyDeclarations().size());
+		
+	}
+	
+	
+	private ResourceSchema createSchema() {
+        ResourceSchemaImpl schema = new ResourceSchemaImpl(
+        		new SimpleResourceID("http://lf.de#", "Person"));
+        
+        TypeDefinitionImpl p1 = new TypeDefinitionImpl();
+        TypeDefinitionImpl p2 = new TypeDefinitionImpl();
+        TypeDefinitionImpl p3 = new TypeDefinitionImpl();
+        TypeDefinitionImpl p4 = new TypeDefinitionImpl();
+
+        p1.setElementaryDataType(ElementaryDataType.STRING);
+        p2.setElementaryDataType(ElementaryDataType.STRING);
+        p3.setElementaryDataType(ElementaryDataType.INTEGER);
+        p4.setElementaryDataType(ElementaryDataType.RESOURCE);
+
+        p2.addConstraint(ConstraintBuilder.buildConstraint(".*@.*"));
+        p4.addConstraint(ConstraintBuilder.buildConstraint(schema
+                .getDescribedType()));
+
+        PropertyDeclarationImpl pa1 = new PropertyDeclarationImpl(
+                new SimpleResourceID("http://lichtflut.de#", "hatGeburtstag"),
+                p1);
+        PropertyDeclarationImpl pa2 = new PropertyDeclarationImpl(
+                new SimpleResourceID("http://lichtflut.de#", "hatEmail"), p2);
+        PropertyDeclarationImpl pa3 = new PropertyDeclarationImpl(
+                new SimpleResourceID("http://lichtflut.de#", "hatAlter"), p3);
+        PropertyDeclarationImpl pa4 = new PropertyDeclarationImpl(
+                new SimpleResourceID("http://lichtflut.de#", "hatKind"), p4);
+
+        pa1.setCardinality(CardinalityBuilder.hasExcactlyOne());
+        pa2.setCardinality(CardinalityBuilder.hasAtLeastOneUpTo(2));
+        pa3.setCardinality(CardinalityBuilder.hasExcactlyOne());
+        pa4.setCardinality(CardinalityBuilder.hasOptionalOneToMany());
+        
+        pa1.setFieldLabelDefinition(new FieldLabelDefinitionImpl("hatGeburtstag"));
+        pa2.setFieldLabelDefinition(new FieldLabelDefinitionImpl("hatEmail"));
+        pa3.setFieldLabelDefinition(new FieldLabelDefinitionImpl("hatAlter"));
+        pa4.setFieldLabelDefinition(new FieldLabelDefinitionImpl("hatKind"));
+
+        schema.addPropertyDeclaration(pa1);
+        schema.addPropertyDeclaration(pa2);
+        schema.addPropertyDeclaration(pa3);
+        schema.addPropertyDeclaration(pa4);
+
+        return schema;
+    }
+
+}
