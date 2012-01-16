3eb3178ced90203b3144f9f19ab2d87bff2190a1
Ravi Knox <rknox@lichtflut.de>
2012-01-16 15:56:38 +0100

    changes in ChangePasswordPanel

--- webck/src/main/java/de/lichtflut/rb/webck/components/organizer/ChangePasswordPanel.java
+++ webck/src/main/java/de/lichtflut/rb/webck/components/organizer/ChangePasswordPanel.java
@@ -7,9 +7,8 @@ import static de.lichtflut.rb.webck.behaviors.ConditionalBehavior.visibleIf;
 import static de.lichtflut.rb.webck.models.ConditionalModel.areEqual;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxFallbackButton;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.validation.EqualPasswordInputValidator;
@@ -18,70 +17,74 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.arastreju.sge.security.User;
 
+import de.lichtflut.rb.webck.models.CurrentUserModel;
+
 /**
- * [TODO Insert description here.]
- * 
+ * <p>
+ * This {@link Panel} allows a user to change his password.
+ * </p><p>
  * Created: Jan 3, 2012
- *
+ * </p>
  * @author Ravi Knox
  */
 @SuppressWarnings("rawtypes")
 public class ChangePasswordPanel extends Panel {
 
-	private IModel<User> user;
+	private IModel<String> newPassword;
 	private IModel<Mode> mode = new Model<Mode>(Mode.VIEW);
-	private WebMarkupContainer container;
+	private IModel<String> mirroredPassword;
 
 	private enum Mode {
 		EDIT, VIEW;
 	}
 	
+	// ------------------------------------------------------
+	
 	/**
-	 * @param id
-	 * @param model
+	 * Constructor.
+	 * @param id - wicket:id
+	 * @param model - a {@link IModel} wrapping a {@link User}
 	 */
-	public ChangePasswordPanel(String id, Model<User> user) {
+	public ChangePasswordPanel(String id) {
 		super(id);
-		this.user = user;
-		container = new WebMarkupContainer("container");
-		container.setOutputMarkupId(true);
 		Form form = new Form("form");
-		createContainer(form);
+		
+		populateForm(form);
 		form.add(createSaveButton("save", form));
 		form.add(createEditButton("edit", form));
 		this.add(form);
 	}
 
 	/**
-	 * @param string
+	 * Populate the form.
 	 * @param form
-	 * @return
 	 */
-	private void createContainer(Form form) {
-		
-		if(mode.getObject().equals(Mode.VIEW)){
-			container.removeAll();
-			container.add(new Label("password", Model.of("Password")));
-			
-		}else{
-			container.removeAll();
-			container.add(new PasswordPanel("password", user));
-		}
+	private void populateForm(Form form) {
+		WebMarkupContainer container = new WebMarkupContainer("container");
+		PasswordTextField current = new PasswordTextField("current");
+		PasswordTextField newPassword = new PasswordTextField("newPassword");
+		PasswordTextField confirmPassword = new PasswordTextField("confirmPassword");
+		container.add(current, newPassword, confirmPassword);
+		container.add(new EqualPasswordInputValidator(newPassword, confirmPassword));
+		container.add(visibleIf(areEqual(mode, Mode.EDIT)));
 		form.add(container);
 	}
 
 	/**
+	 * @param string - wicket:id 
 	 * @param form 
-	 * @param string 
-	 * @return
+	 * @return a {@link AjaxButton}
 	 */
-	private AjaxFallbackButton createSaveButton(String id, Form form) {
-		AjaxFallbackButton save = new AjaxFallbackButton(id, form) {
-
+	private AjaxButton createSaveButton(String id, Form form) {
+		AjaxButton save = new AjaxButton(id, form) {
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				// TODO: implement
+				CurrentUserModel user = new CurrentUserModel();
+				if(verify(user, mirroredPassword)){
+					setNewPassword(user, newPassword);
+				}
 				mode.setObject(Mode.VIEW);
-				createContainer(form);
 				target.add(form);
 			}
 
@@ -94,17 +97,33 @@ public class ChangePasswordPanel extends Panel {
 	}
 
 	/**
-	 * @param form 
-	 * @param string 
+	 * Verifies the current password with the typed one.
+	 * @param user
+	 * @param newPassword
 	 * @return
 	 */
-	private AjaxFallbackButton createEditButton(String id, Form form) {
-		AjaxFallbackButton edit = new AjaxFallbackButton(id, form) {
+	private boolean verify(IModel<User> user, IModel<String> mirroredPassword) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	private void setNewPassword(IModel<User> user, IModel<String> newPassword) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @param string - wicket:id 
+	 * @param form 
+	 * @return a {@link AjaxButton}
+	 */
+	private AjaxButton createEditButton(String id, Form form) {
+		AjaxButton edit = new AjaxButton(id, form) {
 
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
 				mode.setObject(Mode.EDIT);
-				createContainer(form);
 				target.add(form);
 			}
 
@@ -114,27 +133,5 @@ public class ChangePasswordPanel extends Panel {
 		};
 		edit.add(visibleIf(areEqual(mode, Mode.VIEW)));
 		return edit;
-	}
-
-	private class PasswordPanel extends Panel{
-
-		final String PASSWORD_PATTERN = "";
-		
-		/**
-		 * @param id - wicket:id
-		 * @param model
-		 */
-		public PasswordPanel(String id, IModel<?> model) {
-			super(id, model);
-			Form form = new Form("changePasswordForm");
-			PasswordTextField current = new PasswordTextField("current");
-			PasswordTextField newPassword = new PasswordTextField("newPassword");
-			PasswordTextField confirmPassword = new PasswordTextField("confirmPassword");
-			form.add(current, newPassword, confirmPassword);
-			form.add(new EqualPasswordInputValidator(newPassword, confirmPassword));
-			form.add(current);
-			this.add(form);
-		}
-		
 	}
 }
--- webck/src/main/java/de/lichtflut/rb/webck/components/organizer/SetUserProfilePanel.java
+++ webck/src/main/java/de/lichtflut/rb/webck/components/organizer/SetUserProfilePanel.java
@@ -14,7 +14,6 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
