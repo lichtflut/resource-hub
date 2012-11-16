b8352f4bd64596c0f69e643370efd67c71a7d023
Oliver Tigges <oliver.tigges@lichtflut.de>
2012-11-16 11:39:12 +0100

    Optimize imports.

--- app-foundation/src/main/java/de/lichtflut/rb/application/base/LoginPage.java
+++ app-foundation/src/main/java/de/lichtflut/rb/application/base/LoginPage.java
@@ -3,33 +3,14 @@
  */
 package de.lichtflut.rb.application.base;
 
-import de.lichtflut.rb.application.RBApplication;
-import de.lichtflut.rb.application.common.RBPermission;
 import de.lichtflut.rb.application.custom.RequestAccountPage;
 import de.lichtflut.rb.application.custom.ResetPasswordPage;
-import de.lichtflut.rb.application.extensions.ServiceContextInitializer;
-import de.lichtflut.rb.application.pages.AbstractBasePage;
-import de.lichtflut.rb.core.security.AuthModule;
-import de.lichtflut.rb.core.security.AuthenticationService;
 import de.lichtflut.rb.core.security.LoginData;
-import de.lichtflut.rb.core.security.RBUser;
-import de.lichtflut.rb.webck.common.CookieAccess;
-import de.lichtflut.rb.webck.common.RBWebSession;
 import de.lichtflut.rb.webck.components.login.LoginPanel;
 import de.lichtflut.rb.webck.models.infra.VersionInfoModel;
-import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.request.cycle.RequestCycle;
-import org.apache.wicket.request.http.WebRequest;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.arastreju.sge.security.LoginException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Set;
 
 /**
  * <p>
@@ -37,7 +18,7 @@ import java.util.Set;
  * </p>
  * 
  * <p>
- * Created Dec 8, 2011
+ *  Created Dec 8, 2011
  * </p>
  * 
  * @author Oliver Tigges
