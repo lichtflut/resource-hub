05e6b61df6a69c0c42ecf79dcf0fdc69307d6ec2
Ravi Knox <rknox@lichtflut.de>
2013-03-15 20:11:37 +0100

    QuickInfo - fixed diplay value of resources - refactor EntityRowDisplayPanel

--- webck/src/main/java/de/lichtflut/rb/webck/components/entity/quickinfo/QuickInfoPanel.java
+++ webck/src/main/java/de/lichtflut/rb/webck/components/entity/quickinfo/QuickInfoPanel.java
@@ -4,21 +4,28 @@
 package de.lichtflut.rb.webck.components.entity.quickinfo;
 
 import static de.lichtflut.rb.webck.behaviors.ConditionalBehavior.visibleIf;
-import static de.lichtflut.rb.webck.models.ConditionalModel.not;
 
+import java.util.List;
+
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.model.util.ListModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.arastreju.sge.model.ResourceID;
+import org.arastreju.sge.model.nodes.ResourceNode;
 
 import de.lichtflut.rb.core.entity.RBEntity;
 import de.lichtflut.rb.core.entity.RBField;
-import de.lichtflut.rb.webck.components.common.TypedPanel;
+import de.lichtflut.rb.core.schema.model.Datatype;
+import de.lichtflut.rb.core.services.SemanticNetworkService;
 import de.lichtflut.rb.webck.components.entity.EntityRowDisplayPanel;
 import de.lichtflut.rb.webck.models.ConditionalModel;
+import de.lichtflut.rb.webck.models.fields.RBFieldValueModel;
 
 /**
  * <p>
@@ -28,7 +35,12 @@ import de.lichtflut.rb.webck.models.ConditionalModel;
  *
  * @author Ravi Knox
  */
-public class QuickInfoPanel extends TypedPanel<RBEntity> {
+public class QuickInfoPanel extends Panel{
+
+	@SpringBean
+	private SemanticNetworkService networkService;
+
+	// ---------------- Constructor -------------------------
 
 	/**
 	 * Constructor.
@@ -36,22 +48,16 @@ public class QuickInfoPanel extends TypedPanel<RBEntity> {
 	 * @param id The component id
 	 * @param model Contains the entity's resourceId
 	 */
-	public QuickInfoPanel(final String id, final IModel<RBEntity> model) {
-		super(id, model);
-
+	public QuickInfoPanel(final String id, final IModel<List<RBField>> fields) {
+		super(id, fields);
 		WebMarkupContainer container = new WebMarkupContainer("container");
 
 		Label label = new Label("noInfo", new ResourceModel("label.no-info"));
-		label.add(visibleIf(not(hasQuickInfo(model))));
+		label.add(visibleIf(ConditionalModel.isEmpty(fields)));
 		container.add(label);
 
-		ListView<RBField> view = new ListView<RBField>("info", new ListModel<RBField>(model.getObject().getQuickInfo())) {
-			@Override
-			protected void populateItem(final ListItem<RBField> item) {
-				item.add(new EntityRowDisplayPanel("row", item.getModel()));
-			}
-		};
-		view.add(visibleIf(hasQuickInfo(model)));
+		ListView<RBField> view = createListView(fields);
+		view.add(visibleIf(ConditionalModel.isNotEmpty(fields)));
 		container.add(view);
 
 		add(container);
@@ -59,16 +65,30 @@ public class QuickInfoPanel extends TypedPanel<RBEntity> {
 
 	// ------------------------------------------------------
 
-	private ConditionalModel<Boolean> hasQuickInfo(final IModel<RBEntity> model){
-		return new ConditionalModel<Boolean>(model) {
+	protected ListView<RBField> createListView(final IModel<List<RBField>> fields) {
+		ListView<RBField> view = new ListView<RBField>("info", fields) {
 			@Override
-			public boolean isFulfilled() {
-				if (model.getObject() == null || model.getObject().getQuickInfo().isEmpty()) {
-					return false;
-				}
-				return true;
+			protected void populateItem(final ListItem<RBField> item) {
+				EntityRowDisplayPanel panel = createEntityRowDisplayPanel(item);
+				item.add(panel);
+			}
+
+			protected EntityRowDisplayPanel createEntityRowDisplayPanel(final ListItem<RBField> item) {
+				EntityRowDisplayPanel panel = new EntityRowDisplayPanel("row", item.getModel()){
+					@Override
+					protected Component addValueField(final ListItem<RBFieldValueModel> value, final Datatype dataType) {
+						if(Datatype.RESOURCE.name().equals(dataType.name())){
+							ResourceNode resolved = networkService.resolve((ResourceID) value.getModelObject().getFieldValue().getValue());
+							value.getModelObject().setObject(resolved);
+						}
+						return super.addValueField(value, dataType);
+					}
+				};
+				return panel;
 			}
 		};
+		return view;
 	}
 
 }
+
