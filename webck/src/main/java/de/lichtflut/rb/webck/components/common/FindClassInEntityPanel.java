37f327e6da57b6c358b5fa33c0bcd448fc4a1b19
Ravi Knox <rknox@lichtflut.de>
2013-03-18 18:37:41 +0100

    some improvements

--- webck/src/main/java/de/lichtflut/rb/webck/components/common/FindClassInEntityPanel.java
+++ webck/src/main/java/de/lichtflut/rb/webck/components/common/FindClassInEntityPanel.java
@@ -3,12 +3,20 @@
  */
 package de.lichtflut.rb.webck.components.common;
 
+import static de.lichtflut.rb.webck.behaviors.ConditionalBehavior.visibleIf;
+import static de.lichtflut.rb.webck.models.ConditionalModel.and;
+import static de.lichtflut.rb.webck.models.ConditionalModel.isEmpty;
+import static de.lichtflut.rb.webck.models.ConditionalModel.isNotEmpty;
+import static de.lichtflut.rb.webck.models.ConditionalModel.isTrue;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -27,14 +35,12 @@ import de.lichtflut.rb.core.schema.model.PropertyDeclaration;
 import de.lichtflut.rb.core.schema.model.ResourceSchema;
 import de.lichtflut.rb.core.services.SchemaManager;
 import de.lichtflut.rb.core.services.SemanticNetworkService;
-import de.lichtflut.rb.webck.behaviors.ConditionalBehavior;
 import de.lichtflut.rb.webck.common.RBAjaxTarget;
 import de.lichtflut.rb.webck.components.fields.ClassPickerField;
 import de.lichtflut.rb.webck.components.fields.EntityPickerField;
 import de.lichtflut.rb.webck.components.listview.ColumnConfiguration;
 import de.lichtflut.rb.webck.components.listview.ListAction;
 import de.lichtflut.rb.webck.components.listview.ResourceListPanel;
-import de.lichtflut.rb.webck.models.ConditionalModel;
 
 /**
  * <p>
@@ -55,6 +61,7 @@ public class FindClassInEntityPanel extends Panel {
 	private final IModel<ResourceID> wanted;
 	private final IModel<ResourceID> target;
 	private final IModel<List<ResourceNode>> result;
+	private IModel<Boolean> clicked;
 
 	// ---------------- Constructor -------------------------
 
@@ -67,6 +74,7 @@ public class FindClassInEntityPanel extends Panel {
 		this.wanted = new Model<ResourceID>();
 		this.target = new Model<ResourceID>();
 		this.result = new ListModel<ResourceNode>(new ArrayList<ResourceNode>());
+		this.clicked = new Model<Boolean>(Boolean.FALSE);
 
 		initPanel();
 	}
@@ -90,26 +98,35 @@ public class FindClassInEntityPanel extends Panel {
 		add(new PanelTitle("header", new ResourceModel("header.title")));
 
 		Form<?> form = new Form<Void>("form");
+		form.add(new FeedbackPanel("feedbackPanel"));
 		form.add(new ClassPickerField("classPicker", wanted).setRequired(true));
 		form.add(new EntityPickerField("entityPicker", target).setRequired(true));
 		form.add(new AjaxButton("search"){
 			@Override
 			protected void onSubmit(final AjaxRequestTarget target, final Form<?> form) {
+				clicked.setObject(true);
 				buildResult();
 				RBAjaxTarget.add(FindClassInEntityPanel.this);
 			}
 		});
 		add(form);
 		createResultList("result");
+		createNoResoultsFound("noResults");
 
 		setOutputMarkupId(true);
+	}
+
+	private void createNoResoultsFound(final String id) {
+		Label label = new Label(id, new ResourceModel("label.no-results"));
+		label.add(visibleIf(and(isTrue(clicked), isEmpty(result))));
+		add(label);
 	}
 
 	private void createResultList(final String string) {
 		ColumnConfiguration conf = new ColumnConfiguration(ListAction.VIEW);
 		conf.addColumnByPredicate(RDFS.LABEL);
 		ResourceListPanel list = new ResourceListPanel(string, result, new Model<ColumnConfiguration>(conf));
-		list.add(ConditionalBehavior.visibleIf(ConditionalModel.isNotEmpty(result)));
+		list.add(visibleIf(and(isTrue(clicked), isNotEmpty(result))));
 		add(list);
 	}
 
@@ -127,22 +144,22 @@ public class FindClassInEntityPanel extends Panel {
 		SNClass schemaType = SchemaIdentifyingType.of(node);
 		ResourceSchema schema = schemaManager.findSchemaForType(schemaType);
 
+		if (null != schema) {
 
-		for (PropertyDeclaration decl : schema.getPropertyDeclarations()) {
-			if(decl.getConstraint() != null){
-				if(decl.getConstraint().getTypeConstraint() != null){
-					if (!SNOPS.objects(node, decl.getPropertyDescriptor()).isEmpty()) {
-						ResourceID typeConstraint = decl.getConstraint().getTypeConstraint();
-						SNClass typeConstraintSchema = SchemaIdentifyingType.of(typeConstraint);
-						for (ResourceNode resourceNode : SNOPS.objectsAsResources(node, decl.getPropertyDescriptor())) {
+			for (PropertyDeclaration decl : schema.getPropertyDeclarations()) {
+				if(decl.getConstraint() != null){
+					if(decl.getConstraint().getTypeConstraint() != null){
+						if (!SNOPS.objects(node, decl.getPropertyDescriptor()).isEmpty()) {
+							for (ResourceNode resourceNode : SNOPS.objectsAsResources(node, decl.getPropertyDescriptor())) {
 
-							for (ResourceNode  rdfType : SNOPS.objectsAsResources(resourceNode, RDF.TYPE)) {
-								if(rdfType.equals(wanted.getObject())){
-									instances.add(resourceNode);
+								for (ResourceNode  rdfType : SNOPS.objectsAsResources(resourceNode, RDF.TYPE)) {
+									if(rdfType.equals(wanted.getObject())){
+										instances.add(resourceNode);
 
+									}
 								}
+								findInstancesRecursive(resourceNode, instances);
 							}
-							findInstancesRecursive(resourceNode, instances);
 						}
 					}
 				}
