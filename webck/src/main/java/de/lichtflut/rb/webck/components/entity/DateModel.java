7c819defea303d2dd2cc339b5a3bccaf5aba11f1
Ravi Knox <rknox@lichtflut.de>
2013-04-08 14:20:46 +0200

    Improved DateModel

--- webck/src/main/java/de/lichtflut/rb/webck/components/entity/DateModel.java
+++ webck/src/main/java/de/lichtflut/rb/webck/components/entity/DateModel.java
@@ -3,10 +3,15 @@
  */
 package de.lichtflut.rb.webck.components.entity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.wicket.model.IModel;
+import org.arastreju.sge.model.ElementaryDataType;
 import org.arastreju.sge.model.nodes.SNValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.lichtflut.rb.webck.conversion.RBDateConverter;
 import de.lichtflut.rb.webck.models.fields.RBFieldValueModel;
@@ -21,6 +26,8 @@ import de.lichtflut.rb.webck.models.fields.RBFieldValueModel;
  * @author Ravi Knox
  */
 public class DateModel implements IModel<Date> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DateModel.class);
 
 	private final RBFieldValueModel value;
 
@@ -39,11 +46,28 @@ public class DateModel implements IModel<Date> {
 
 	@Override
 	public Date getObject() {
+		Date date = null;
 		if(null == value || null == value.getObject()){
-			return null;
+			return date;
 		}
-		Date timeValue = ((SNValue)value.getFieldValue().getValue()).getTimeValue();
-		return new Date(timeValue.getTime());
+		// If date was stored as a string, try arastrejus default date format
+		ElementaryDataType dataType = ((SNValue)value.getFieldValue().getValue()).getDataType();
+		if(ElementaryDataType.STRING.name().equals(dataType.name())){
+			try {
+				date = new SimpleDateFormat("yyyy-MM-dd").parse(((SNValue)value.getFieldValue().getValue()).getStringValue());
+			} catch (ParseException e) {
+				LOGGER.error("Could not parse date using format: yyyy-MM-dd. Will attempt another format");
+			}
+			try {
+				date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(((SNValue)value.getFieldValue().getValue()).getStringValue());
+			} catch (ParseException e) {
+				LOGGER.error("Could not parse date using format:yyyy-MM-dd'T'HH:mm:ssZ.");
+			}
+		}else{
+			Date timeValue = ((SNValue)value.getFieldValue().getValue()).getTimeValue();
+			date = new Date(timeValue.getTime());
+		}
+		return date;
 	}
 
 	@Override
