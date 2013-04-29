b40c981767dc6d8359fb5f60ff42f77ad7faecaf
Oliver Tigges <oliver.tigges@lichtflut.de>
2013-04-29 20:35:18 +0200

    Global update of license headers.

--- webck/src/main/java/de/lichtflut/rb/webck/components/rteditor/jquery.wysiwyg.js
+++ webck/src/main/java/de/lichtflut/rb/webck/components/rteditor/jquery.wysiwyg.js
@@ -1,16 +1,18 @@
-/**
- * WYSIWYG - jQuery plugin 0.97
- * (0.97.2 - From infinity)
+/*
+ * Copyright (C) 2013 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
  *
- * Copyright (c) 2008-2009 Juan M Martinez, 2010-2011 Akzhan Abdulin and all contributors
- * https://github.com/akzhan/jwysiwyg
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 /*jslint browser: true, forin: true */
 
 (function ($) {
