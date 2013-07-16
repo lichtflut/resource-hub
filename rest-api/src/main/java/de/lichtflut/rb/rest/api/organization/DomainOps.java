31787fe8e96589095712bf060556aa873b911208
Oliver Tigges <oliver.tigges@lichtflut.de>
2013-07-16 12:59:16 +0200

    Prepared refactoring of domain Ops.

--- rest-api/src/main/java/de/lichtflut/rb/rest/api/organization/DomainOps.java
+++ rest-api/src/main/java/de/lichtflut/rb/rest/api/organization/DomainOps.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.lichtflut.rb.rest.api;
+package de.lichtflut.rb.rest.api.organization;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -27,6 +27,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import de.lichtflut.rb.core.eh.UnauthenticatedUserException;
+import de.lichtflut.rb.rest.api.RBServiceEndpoint;
 import org.arastreju.sge.persistence.TransactionControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,8 +47,7 @@ import java.util.Arrays;
 
 /**
  * <p>
- * 
- * 
+ *  HTTP Resource for operations on domains.
  * </p>
  * 
  * @author Nils Bleisch (nbleisch@lichtflut.de)
@@ -66,32 +66,32 @@ public class DomainOps extends RBServiceEndpoint {
 
 	@DELETE
 	@RBOperation(type = TYPE.DOMAIN_DELETE)
-	public Response deleteDomain(@PathParam(DOMAIN_ID_PARAM) String domainID,
-			@QueryParam(AUTH_TOKEN) String token) throws UnauthenticatedUserException {
+	public Response deleteDomain(
+            @PathParam(DOMAIN_ID_PARAM) String domainID,
+			@QueryParam(AUTH_TOKEN) String token)
+            throws UnauthenticatedUserException {
 		RBUser user = authenticateUser(token);
+
 		if (!getAuthHandler().isAuthorized(user, domainID)) {
 			return Response.status(Status.UNAUTHORIZED).build();
 		}
+
 		try {
 			DomainManager domainManager = this.authModule.getDomainManager();
 			RBDomain rbDomain = domainManager.findDomain(domainID);
 			if (rbDomain != null) {
 				domainManager.deleteDomain(rbDomain);
 			}
-			return Response.status(Status.CREATED).build();
+			return Response.status(Status.NO_CONTENT).build();
 		} catch (Exception any) {
-			LOGGER.error(
-					"The domain "
-							+ domainID
-							+ " couldnt be deleted tue to the following exception",
-					any);
+			LOGGER.error("The domain {} couldnt be deleted tue to the following exception", domainID);
+            LOGGER.error("Error while deleting domain.",any);
 			return Response.serverError().build();
 		}
 
 	}
 
 	/**
-	 * <p>
 	 * Create a new domain under the following conditions.
 	 * 
 	 * <ul>
@@ -119,8 +119,12 @@ public class DomainOps extends RBServiceEndpoint {
 	@POST
 	@RBOperation(type = TYPE.DOMAIN_CREATE)
 	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Response createDomain(@PathParam(DOMAIN_ID_PARAM) String domainID,
-			@QueryParam(AUTH_TOKEN) String token, SystemDomain domain) throws UnauthenticatedUserException {
+	public Response createDomain(
+            @PathParam(DOMAIN_ID_PARAM) String domainID,
+			@QueryParam(AUTH_TOKEN) String token,
+            SystemDomain domain)
+        throws UnauthenticatedUserException
+    {
 
 		// Check the equality of domainID's
 		if (domain != null && domain.getDomainIdentifier() != null
@@ -160,17 +164,98 @@ public class DomainOps extends RBServiceEndpoint {
 				try {
 					addDomainAdmin(rbDomain, admin, provider);
 				} catch (RBAuthException e) {
-					LOGGER.error("Domain admin couldnt be created due to the following exception", e);
+					LOGGER.error("Domain admin could not be created due to the following exception", e);
 					tx.fail();
 				}
 			}
 		} catch (Exception any) {
 			LOGGER.error("Domain admin couldnt be created due to the following exception",any);
 			tx.fail();
-		}
-		tx.finish();
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		} finally {
+            tx.finish();
+        }
+
 		return Response.status(Status.CREATED).build();
 	}
+
+    /**
+     * <p>
+     * Updates an existing domain under the following conditions.
+     *
+     * <li>Update
+     * <ul>
+     * <li>Is processed when the given domainID does already exists</li>
+     * <li>The user has to be domain-admin or root</li>
+     * <li>A {@link SystemDomain} must be given to process the update</li>
+     * <li>if {@link de.lichtflut.rb.rest.api.models.generate.SystemDomain#getDomainIdentifier()} is not null, it must be equal
+     * to the given domainID delivered as PathParam</li>
+     * <li>If the given domain administrator does not exists, a new system user
+     * will be created so long the password and email address is given</li>
+     * </ul>
+     * </li>
+     * </p>
+     *
+     * @param domainID
+     * @param token
+     * @param domain
+     * @return
+     */
+    @PUT
+    @RBOperation(type = TYPE.DOMAIN_UPDATE)
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response updateDomain(@PathParam(DOMAIN_ID_PARAM) String domainID,
+                                 @QueryParam(AUTH_TOKEN) String token, SystemDomain domain) throws UnauthenticatedUserException {
+        // Check if domain is not null
+        if (domain == null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+        // Check the equality of domainID's
+        if (domain.getDomainIdentifier() != null
+                && (!domain.getDomainIdentifier().equals(domainID))) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        // Authenticate the user
+        RBUser user = authenticateUser(token);
+        if (!getAuthHandler().isAuthorized(user, domainID)) {
+            return Response.status(Status.UNAUTHORIZED).build();
+        }
+        DomainManager domainManager = this.authModule.getDomainManager();
+        ServiceProvider provider = getProvider(domainID, user);
+        RBDomain rbDomain = domainManager.findDomain(domainID);
+        if (rbDomain == null) {
+            return Response.status(Status.CONFLICT).build();
+        }
+        rbDomain.setDescription(domain.getDescription());
+        rbDomain.setTitle(domain.getTitle());
+        rbDomain.setName(domain.getDomainIdentifier());
+        SystemIdentity admin = domain.getDomainAdministrator();
+        if (admin != null
+                && (admin.getId() == null || admin.getPassword() == null)) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        domainManager.updateDomain(rbDomain);
+
+        // Lets process the
+        if (admin != null) {
+            try {
+                provider.getSecurityService()
+                        .createDomainAdmin(rbDomain, admin.getId(),
+                                admin.getUsername(), admin.getPassword());
+            } catch (RBAuthException e) {
+                LOGGER.error(
+                        "Domain admin couldnt be created due to the following exception",
+                        e);
+            }
+        }
+
+        return Response.status(Status.CREATED).build();
+
+    }
+
+    // ----------------------------------------------------
 
 	/**
 	 * @param rbDomain
@@ -188,82 +273,6 @@ public class DomainOps extends RBServiceEndpoint {
 			uManager.setUserRoles(user, rbDomain.getName(),
 					Arrays.asList(new String[0]));
 		}
-	}
-
-	/**
-	 * <p>
-	 * Updates an existing domain under the following conditions.
-	 * 
-	 * <li>Update
-	 * <ul>
-	 * <li>Is processed when the given domainID does already exists</li>
-	 * <li>The user has to be domain-admin or root</li>
-	 * <li>A {@link SystemDomain} must be given to process the update</li>
-	 * <li>if {@link de.lichtflut.rb.rest.api.models.generate.SystemDomain#getDomainIdentifier()} is not null, it must be equal
-	 * to the given domainID delivered as PathParam</li>
-	 * <li>If the given domain administrator does not exists, a new system user
-	 * will be created so long the password and email address is given</li>
-	 * </ul>
-	 * </li>
-	 * </p>
-	 * 
-	 * @param domainID
-	 * @param token
-	 * @param domain
-	 * @return
-	 */
-	@PUT
-	@RBOperation(type = TYPE.DOMAIN_UPDATE)
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Response updateDomain(@PathParam(DOMAIN_ID_PARAM) String domainID,
-			@QueryParam(AUTH_TOKEN) String token, SystemDomain domain) throws UnauthenticatedUserException {
-		// Check if domain is not null
-		if (domain == null) {
-			return Response.status(Status.BAD_REQUEST).build();
-		}
-		// Check the equality of domainID's
-		if (domain.getDomainIdentifier() != null
-				&& (!domain.getDomainIdentifier().equals(domainID))) {
-			return Response.status(Status.BAD_REQUEST).build();
-		}
-
-		// Authenticate the user
-		RBUser user = authenticateUser(token);
-		if (!getAuthHandler().isAuthorized(user, domainID)) {
-			return Response.status(Status.UNAUTHORIZED).build();
-		}
-		DomainManager domainManager = this.authModule.getDomainManager();
-		ServiceProvider provider = getProvider(domainID, user);
-		RBDomain rbDomain = domainManager.findDomain(domainID);
-		if (rbDomain == null) {
-			return Response.status(Status.CONFLICT).build();
-		}
-		rbDomain.setDescription(domain.getDescription());
-		rbDomain.setTitle(domain.getTitle());
-		rbDomain.setName(domain.getDomainIdentifier());
-		SystemIdentity admin = domain.getDomainAdministrator();
-		if (admin != null
-				&& (admin.getId() == null || admin.getPassword() == null)) {
-			return Response.status(Status.BAD_REQUEST).build();
-		}
-
-		domainManager.updateDomain(rbDomain);
-
-		// Lets process the
-		if (admin != null) {
-			try {
-				provider.getSecurityService()
-						.createDomainAdmin(rbDomain, admin.getId(),
-								admin.getUsername(), admin.getPassword());
-			} catch (RBAuthException e) {
-				LOGGER.error(
-						"Domain admin couldnt be created due to the following exception",
-						e);
-			}
-		}
-
-		return Response.status(Status.CREATED).build();
-
 	}
 
 }
